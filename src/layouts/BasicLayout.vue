53966f8759891c5b5f2232de6db8a1c237fa1ea5
丁一鸣 <17081288@cnsuning.com>
2019-11-18 22:15:40 +0800

    【layout】remove SettingDrawer

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -52,8 +52,6 @@
         <global-footer />
       </a-layout-footer>
 
-      <!-- Setting Drawer (show in development mode) -->
-      <setting-drawer v-if="!production"></setting-drawer>
     </a-layout>
   </a-layout>
 
@@ -69,7 +67,6 @@ import RouteView from './RouteView'
 import SideMenu from '@/components/Menu/SideMenu'
 import GlobalHeader from '@/components/GlobalHeader'
 import GlobalFooter from '@/components/GlobalFooter'
-import SettingDrawer from '@/components/SettingDrawer'
 
 export default {
   name: 'BasicLayout',
@@ -79,7 +76,6 @@ export default {
     SideMenu,
     GlobalHeader,
     GlobalFooter,
-    SettingDrawer
   },
   data () {
     return {
