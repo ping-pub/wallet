fe337f29fc80308a0b64307fa1ba8c33613017b9
丁一鸣 <17081288@cnsuning.com>
2019-12-05 19:53:15 +0800

    【goverance】修改布局

--- src/views/Goverance.vue
+++ src/views/Goverance.vue
@@ -10,139 +10,28 @@
       <el-button icon="el-icon-search">搜索</el-button>
     </div>
 
-    <el-table size="large" :show-header="false" class="mb-16" :data="tableData">
-      <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column prop="name">
-        <template slot-scope="scope">
-          <p style="font-size: 16px;">{{scope.row.name}}</p>
-          <p style="font-size: 12px;color: #999;">{{ scope.row.description }}</p>
-        </template>
-      </el-table-column>
-      <el-table-column align="right" prop="votingPower" width="60">
-        <template slot-scope="scope">
-          <div>No</div>
-          <div>9.8%</div>
-        </template>
-      </el-table-column>
-      <el-table-column align="right" prop="votingPower" width="60">
-        <template slot-scope="scope">
-          <div>No</div>
-          <div>9.8%</div>
-        </template>
-      </el-table-column>
-      <el-table-column align="right" prop="votingPower" width="60">
-        <template slot-scope="scope">
-          <div>No</div>
-          <div>9.8%</div>
-        </template>
-      </el-table-column>
-      <el-table-column align="right" prop="votingPower" width="60">
-        <template slot-scope="scope">
-          <div>No</div>
-          <div>9.8%</div>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="votingPower" width="100">
-        <template slot-scope="scope">
-          <el-tag type="success">Passed</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column width="200" align="center">
-        <template slot-scope="scope">
-          <el-button size="mini" type="primary">投票</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-card shadow="never" class="mb-8" v-for="n in 10" :key="n">
+      <p class="mb-8" style="color: rgba(0,0,0,.85);
+    font-size: 16px;
+    line-height: 24px;">Adjustment of blocks_per_year to come aligned with actual block time</p>
+      <div class="mb-8">
+        <el-tag size="mini" class="mr-8">ID:1</el-tag>
+        <el-tag size="mini" type="success">Passed</el-tag>
+      </div>
+      <p class="mb-8" style="color: rgba(0,0,0,.65);">This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate. ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z</p>
+      <div class="mb-8">
+        <span>1987-05-03 17:50:03</span>
+      </div>
+      <div>
+        <el-button size="large" type="text">投票</el-button>
+      </div>
+    </el-card>
 
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      tableData: [
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-        {
-          name: "Adjustment of blocks_per_year to come aligned with actual block time",
-          description: "This governance proposal is for adjustment of blocks_per_year parameter to normalize the inflation rate and reward rate.\n ipfs link: https://ipfs.io/ipfs/QmXqEBr56xeUzFpgjsmDKMSit3iqnKaDEL4tabxPXoz9x2019-03-20T06:41:27.040075748Z",
-          votingPower: "884654",
-          rewards: "9.8%"
-        },
-      ]
-    };
-  },
   methods: {
     go(path) {
       this.$router.push(path);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,25 +7,55 @@
 
     <el-row :gutter="16" class="mb-16">
       <el-col :span="6" :xs="24">
-        <el-card shadow="never">tokens</el-card>
+        <el-card shadow="never">
+          <div class="flex-center">
+            <div class="flex-1">
+              <p class="home-subtitle">Bonded Tokens</p>
+              <p class="home-title">126,560</p>
+            </div>
+            <i style="font-size: 36px;color: #66befb;" class="iconfont icon-bonded"></i>
+          </div>
+        </el-card>
       </el-col>
       <el-col :span="6" :xs="24">
-        <el-card shadow="never">tokens</el-card>
+        <el-card shadow="never">
+          <div class="flex-center">
+            <div class="flex-1">
+              <p class="home-subtitle">Total Tokens</p>
+              <p class="home-title">12226,560</p>
+            </div>
+            <i style="font-size: 42px;color: #4cd595;" class="iconfont icon-total"></i>
+          </div>
+        </el-card>
       </el-col>
       <el-col :span="6" :xs="24">
-        <el-card shadow="never">tokens</el-card>
+        <el-card shadow="never">
+          <div class="flex-center">
+            <div class="flex-1">
+              <p class="home-subtitle">Bonded Rate</p>
+              <p class="home-title">12%</p>
+            </div>
+            <i style="font-size: 40px;color: #9c5fdf;" class="iconfont icon-rate"></i>
+          </div>
+        </el-card>
       </el-col>
       <el-col :span="6" :xs="24">
-        <el-card shadow="never">tokens</el-card>
+        <el-card shadow="never">
+          <div class="flex-center">
+            <div class="flex-1">
+              <p class="home-subtitle">Rewards</p>
+              <p class="home-title">12%</p>
+            </div>
+            <i style="font-size: 40px;color: #ff567b;" class="iconfont icon-rewards"></i>
+          </div>
+        </el-card>
       </el-col>
     </el-row>
 
     <div>
       <span style="font-size: 16px;">钱包</span>
 
-      <div>
-        地址、备注、所属人、可用额度、总额度、 市价、一个地址可能有多个币种
-      </div>
+      <div>地址、备注、所属人、可用额度、总额度、 市价、一个地址可能有多个币种</div>
     </div>
   </div>
 </template>
@@ -33,3 +63,18 @@
 <script>
 export default {};
 </script>
+
+<style scoped>
+.home-title {
+  word-break: break-all;
+  white-space: nowrap;
+  margin-top: 4px;
+  margin-bottom: 0;
+  font-size: 24px;
+  line-height: 32px;
+}
+.home-subtitle {
+  font-size: 14px;
+  color: #999;
+}
+</style>
