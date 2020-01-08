7641730d3de23289d1fe34ed6615835fb3b5df94
dingyiming <315129552@qq.com>
2020-01-08 21:22:26 +0800

    【html no-cache】

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var nunjucks = require('nunjucks')
 var path = require('path');
 var app = express();
 var router = express.Router();
@@ -7,35 +6,36 @@ var proxy = require('http-proxy-middleware');
 
 // 根据请求转发
 app.use('/api*', proxy({
-  target: 'https://stargate.cosmos.network',
-  changeOrigin: true,
-  pathRewrite: { ['^/api']: '' },
-  router: function(req) {
-    return req.headers.server;
-  }
+	target: 'https://stargate.cosmos.network',
+	changeOrigin: true,
+	pathRewrite: {
+		['^/api']: ''
+	},
+	router: function(req) {
+		return req.headers.server;
+	}
 }))
+app.set('views', path.join(__dirname, 'h5'));
 
-nunjucks.configure('h5', {
-  autoescape: true,
-  express: app
-});
-
-router.get('/', function (req, res, next) {
-  res.render('index.html');
+router.get('/', function(req, res, next) {
+	res.set({
+		'Cache-Control': 'no-cache'
+	});
+	res.sendFile(__dirname + '/h5/index.html')
 });
 
 app.use('/', router);
 
 app.use(express.static(path.join(__dirname, 'h5'), {
-  maxAge: '1y',
-  expires: '1y',
-  Etag: false,
-  lastModified: false
+	maxAge: '1y',
+	expires: '1y',
+	Etag: false,
+	lastModified: false
 }));
 
-var server = app.listen(80, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(80, function() {
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('app listening at http://%s:%s', host, port);
+	console.log('app listening at http://%s:%s', host, port);
 });
