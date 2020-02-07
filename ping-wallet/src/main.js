3ba9bf87fdcc49b68455ca0954db12b90487b642
dingyiming <315129552@qq.com>
2020-02-07 23:56:49 +0800

    vue i18n main.js 引入

--- ping-wallet/src/main.js
+++ ping-wallet/src/main.js
@@ -6,24 +6,36 @@ import store from './store'
 import api from './api'
 import Vant, { Toast, Locale } from 'vant';
 import enUS from 'vant/lib/locale/lang/en-US';
+import VueI18n from 'vue-i18n'
 
 import 'vant/lib/index.css';
 import './components'
 
 Vue.use(Vant);
-
-const messages = {
-  'zh-CN': {
-  },
-  'en-US': {
-  }
-};
-
-Locale.add(messages);
+Vue.use(VueI18n)
 
 // zh-CN  en-US
 Locale.use('zh-CN', enUS);
-console.log(Locale)
+
+// 准备翻译的语言环境信息
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  cn: {
+    message: {
+      hello: '你好'
+    }
+  }
+}
+
+// 通过选项创建 VueI18n 实例
+const i18n = new VueI18n({
+  locale: 'cn', // 设置地区
+  messages, // 设置地区信息
+})
 
 Vue.config.productionTip = false
 Vue.prototype.$Toast = Toast
@@ -33,6 +45,7 @@ store.commit('initSetting')
 store.commit('initWallet')
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
