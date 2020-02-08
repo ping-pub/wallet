6b5bbecf2b7a2d1e8ff20606d8273a7d851b445e
dingyiming <315129552@qq.com>
2020-02-08 15:32:10 +0800

    收款详情页国际化

---
+++ ping-wallet/src/components/wallet-receive/mixinLang.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-08 15:12:49
+ * @LastEditTime: 2020-02-08 15:25:05
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /ping-wallet/ping-wallet/src/components/wallet-receive/mixinLang.js
+ */
+export default {
+  i18n: {
+    // `i18n` 选项，为组件设置语言环境信息
+    messages: {
+      en: {
+        message: {
+          Name: "Name",
+          Address: "Address",
+          QAcode: "QR code",
+          Copy: "Copy",
+        }
+      },
+      cn: {
+        message: {
+          Name: "名称",
+          Address: "地址",
+          QA: "二维码",
+          Copy: "复制",
+
+        }
+      }
+    }
+  }
+}
